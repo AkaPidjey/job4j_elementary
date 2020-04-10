a136310cd758b7a3efee0bcf567d7b6e46d84292
AkaPidjey <pidjey@gmail.com>
2020-04-10 12:46:54 +0300

    Поиск индекса в диапазоне.[#261577] Поиск значени в массиве в определенном диапозоне.

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,4 +11,22 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /***
+     * @param data - массив чисел
+     * @param el- элемент, который нужно найти
+     * @param start- индекс, с которого начинаем поиск
+     * @param finish - индекс, которым заканчиваем поиск
+     * @return rst - возвращает индекс найденного элемента, или значение -1, если элемент не найден
+     ***/
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
 }
