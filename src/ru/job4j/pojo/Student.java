943daf1fa404ec25109e3a88c392d60517df25fd
AkaPidjey <pidjey@gmail.com>
2020-04-17 21:14:23 +0300

    Модель данных.[#261744]

---
+++ src/ru/job4j/pojo/Student.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fio;
+    private String gruppa;
+    private String date;
+
+    public String getFio() {
+        return fio;
+    }
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+    public String getGruppa() {
+        return gruppa;
+    }
+    public void setGruppa(String gruppa) {
+        this.gruppa = gruppa;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
