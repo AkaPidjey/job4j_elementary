15ab950f170880b4e74862cad9e211fc41d0c663
AkaPidjey <pidjey@gmail.com>
2020-04-18 14:42:15 +0300

    Рефакторинг - Площадь треугольника.[#261723] вариант 2

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -9,12 +10,52 @@ public class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        boolean result = triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
     @Test
     public void whenNoExist() {
-        boolean result = Triangle.exist(1.0, 3.0, 1.0);
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        boolean result = triangle.exist(1.0, 3.0, 1.0);
         assertThat(result, is(false));
     }
+    @Test
+    public void whenPeriodRavno15() {
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 1.5;
+        double out = triangle.period(1.0, 1.0, 1.0);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void whenPeriodRavno30() {
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 3.0;
+        double out = triangle.period(1.0, 2.0, 3.0);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenAreaRavno196() {
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 3);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 1.96;
+        double out = triangle.area();
+        Assert.assertEquals(expected, out, 0.05);
+    }
+
 }
