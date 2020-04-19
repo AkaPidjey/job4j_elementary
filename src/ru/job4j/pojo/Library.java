4bfd0ee1aabae2e915552e0ca03aa680e868c0e7
AkaPidjey <pidjey@gmail.com>
2020-04-19 11:04:30 +0300

    Массивы и модели.[#261745]

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -7,28 +7,32 @@ public class Library {
         Book book2 = new Book("Bible", 400);
         Book book3 = new Book("Clean code", 350);
 
-        Book[] books = new Book[5];
+        Book[] books = new Book[4];
 
         books[0] = book0;
         books[1] = book1;
         books[2] = book2;
         books[3] = book3;
+        //books[4] = null;
 
+        System.out.println("Список первоначальный");
         for (int i = 0; i < books.length; i++) {
             Book vivod = books[i];
             System.out.println(vivod.getName() + " - " + vivod.getPage());
         }
 
-        books[4] = books[0];
+        Book tmp = books[0];
         books[0] = books[2];
-        books[2] = books[4];
-        books[4] = null;
+        books[2] = tmp;
+        tmp = null;
 
+        System.out.println("Список после перемещения книг");
         for (int i = 0; i < books.length; i++) {
             Book vivod = books[i];
             System.out.println(vivod.getName() + " - " + vivod.getPage());
         }
 
+        System.out.println("Список с выборкой по условию");
         for (int i = 0; i < books.length; i++) {
             Book vivod = books[i];
             if (vivod.getName().equals("Clean code")) {
