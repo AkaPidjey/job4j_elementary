99848897cc2f7337f47618a516c4cf4639b639ae
AkaPidjey <pidjey@gmail.com>
2020-04-22 17:29:05 +0300

    Интерфейс Input[#261694]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+    }
+
+    @Override
+    public void passenger(int pas) {
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        double price;
+        price = fuel * 49;
+        return price;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    public void go();
+    public void passenger(int pas);
+    public double refuel(double fuel);
+}
