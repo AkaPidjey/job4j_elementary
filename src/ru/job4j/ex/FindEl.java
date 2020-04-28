760b0d3db39d62adaf6331b125471d5668b6e8c0
AkaPidjey <pidjey@gmail.com>
2020-04-28 10:05:58 +0300

    Пользовательские исключения.[#261713] вариант 2

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -2,14 +2,16 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if () {
-            throw  new ElementNotFoundException("No value");
-        }
+
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (key == value[i]) {
+            if (key.equals(value[i])) {
                 rsl = i;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("No value");
         }
         return rsl;
 
@@ -17,9 +19,11 @@ public class FindEl {
     }
     public static void main(String[] args) {
         try {
-            indexOf("No value");
+            indexOf(value, rsl);
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
     }
+
+
 }
