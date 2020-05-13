f11eeff6989eb1992f6a6fb0087670f41b63b9a9
AkaPidjey <pidjey@gmail.com>
2020-05-13 20:33:16 +0300

    Тесты на ValidateInput.[#261716] вариант 2

--- test/ru/job4j/tracker/ValidateInputTest.java
+++ test/ru/job4j/tracker/ValidateInputTest.java
@@ -15,10 +15,19 @@ public class ValidateInputTest {
         String[] data = {"one", "1"};
         ValidateInput input = new ValidateStubInput(data);
         input.askInt("Enter");
-        assertThat(mem, is(String.format("Please enter validate data again.%n")));
+        assertThat(mem.toString(), is(String.format("Please enter validate data again.%n")));
         System.setOut(out);
     }
-
-
+    @Test
+    public void whenMaxInput() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        String[] data = {"2", "1", "3"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("1", 2);
+        assertThat(mem.toString(), is(String.format("Please select key from menu.%n")));
+        System.setOut(out);
+    }
 
 }
